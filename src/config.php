e3ea306a87aa0baa1d2bb6e5a6ae47ed154ea882
Rias <hello@rias.be>
2019-02-01 15:10:18 +0000

    Apply fixes from StyleCI

--- src/config.php
+++ src/config.php
@@ -28,24 +28,24 @@ return [
     // Predefined colours
     'colours' => [
         [
-            'label' => 'red',
-            'color' => '#ff0000',
+            'label'   => 'red',
+            'color'   => '#ff0000',
             'default' => false,
         ],
         [
-            'label' => 'green',
-            'color' => '#00ff00',
+            'label'   => 'green',
+            'color'   => '#00ff00',
             'default' => false,
         ],
         [
-            'label' => 'blue',
-            'color' => '#0000ff',
+            'label'   => 'blue',
+            'color'   => '#0000ff',
             'default' => false,
         ],
         [
-            'label' => 'pink',
-            'color' => '#ff00ff',
+            'label'   => 'pink',
+            'color'   => '#ff00ff',
             'default' => false,
-        ]
+        ],
     ],
 ];
