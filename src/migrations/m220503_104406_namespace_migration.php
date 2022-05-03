d28a561af9cbdd19cd6156cf9e4708d4d375bf78
Stefanie Gevaert <>
2022-05-03 13:01:11 +0200

    feat: provided a migration to update the namespace from percipioglobal to percipiolondon

---
+++ src/migrations/m220503_104406_namespace_migration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace percipiolondon\colourswatches\migrations;
+
+use craft\db\Migration;
+
+/**
+ * m200911_142127_update_namespace migration.
+ */
+class m220503_104406_namespace_migration extends Migration
+{
+    /**
+     * @return bool
+     */
+    public function safeUp(): bool
+    {
+        echo "update_namespace updating .\n";
+
+        $this->update(
+            '{{%fields}}',
+            [
+                'type' => 'percipiolondon\\colourswatches\\fields\\ColourSwatches',
+            ],
+            'type = :percipioglobal',
+            [
+                'percipioglobal' => 'percipioglobal\\colourswatches\\fields\\ColourSwatches',
+            ]
+        );
+
+        return true;
+    }
+
+
+    /**
+     * @return false
+     */
+    public function safeDown(): bool
+    {
+        echo "m200911_142127_update_namespace cannot be reverted.\n";
+        return false;
+    }
+}
